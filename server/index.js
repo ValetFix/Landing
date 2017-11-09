937076ac2151b2fe56835337be6fd97a6839d6c3
Dennis Park <dennisinchoonpark@gmail.com>
2017-11-09 12:03:43 -0800

    changed typo in server

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(cors())
 
 app.get('/', function(req, res) {
-  res.render('build/index.html');
+  res.render('index.html');
 })
 
 app.post('/subscribe', (req, res) => {
